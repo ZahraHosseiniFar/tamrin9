93d1ae35004da275d8558aadce82179ec298b04f
ZahraHosseiniFar <117304346+ZahraHosseiniFar@users.noreply.github.com>
2023-01-27 21:04:41 +0330

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car car1 = new car();
+            Console.WriteLine(car1.HELLOUSER(" anashid "));
+            car car2 = new car();
+            Console.WriteLine(car2.HELLOUSER(" ana "));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class car : MYInterface2
+    {
+        public string HELLOUSER(string name)
+        {
+            return "hello" + name;
+        }
+    }
+}
